09e64ac9ab25d3e3b454c68b7d5c751d28d7964d
David Roys <davidroys@icloud.com>
2021-10-30 16:23:10 +1300

    Directory crawling working

--- src/extension/Services/CommandInit.ts
+++ src/extension/Services/CommandInit.ts
@@ -9,6 +9,8 @@ import { Err } from "../Utils/Err";
 
 export class Init {
     async resolve() {
+        let FilesToCopyMap : Map<string, string> = new Map<string, string>();
+
         let NewProjectFilesLocation: string = Config.getNewProjectFilesLocation();
 
         if (NewProjectFilesLocation == '') {
@@ -26,24 +28,48 @@ export class Init {
             vscode.window.showErrorMessage('Folder ' + NewProjectFilesLocation + ' does not exist.');
             return;
         }
-        
+
         var path = require('path');
-        var settingsFileName: string = path.join(NewProjectFilesLocation, '.noob/settings.json');
+        const settingsFolderName: string = path.join(NewProjectFilesLocation, '.noob');
+        const settingsFileName: string = path.join(settingsFolderName, 'settings.json');
         if (fs.existsSync(settingsFileName)) {
-            vscode.window.showInformationMessage('Settings file found at '+settingsFileName);
+            vscode.window.showInformationMessage('Settings file found at ' + settingsFileName);
         } else {
-            vscode.window.showInformationMessage('No settings file found at '+settingsFileName);
+            vscode.window.showInformationMessage('No settings file found at ' + settingsFileName);
         }
+
+        // Copy all files from the folder path to a map but ignore NewProjectFilesLocation.
+        // Iterate through all the subdirectories.
+
+        this.listFilesInDirectory(NewProjectFilesLocation, FilesToCopyMap);
+
+        for (let key of FilesToCopyMap.keys()) {
+            let value = FilesToCopyMap.get(key);
+            OwnConsole.ownConsole.appendLine(key + " -> " + value);
+        }
+
+        // const files = fs.readdirSync(NewProjectFilesLocation);
+
+        // for (const file of files) {
+        //     const fromPath = path.join(NewProjectFilesLocation, file);
+        //     const stat = fs.statSync(fromPath);
+        //     if (!stat.isDirectory()) {
+        //         if (fromPath != settingsFolderName) {
+        //             OwnConsole.ownConsole.appendLine(fromPath);
+        //         }
+        //     }
+        //     OwnConsole.ownConsole.show();
+        // }
 
         // if find a .noob\settings.json
 
-            // read the list of folders that could be used and if there is more than one show a quick pick to get the source folder
-            // read a list of files that should be parsed and have values substituted.
-            // read the path to common files
+        // read the list of folders that could be used and if there is more than one show a quick pick to get the source folder
+        // read a list of files that should be parsed and have values substituted.
+        // read the path to common files
 
         // else
 
-            // set source folder to the settings noob path folder.
+        // set source folder to the settings noob path folder.
 
         // If we have a common files path copy these files.
         // Copy all files from the source folder to the current folder.
@@ -52,59 +78,82 @@ export class Init {
         // possibly need to do a "reload window" type to get this project recognised as an al project.
 
 
-/*
-
-chooseTargetPlatform(dir) {
-        const options = ({
-            canPickMany: false,
-            ignoreFocusOut: true,
-            placeHolder: resources_1.default.targetPlatformPlaceholder
-        });
-        const createItem = (label, description, picked = false) => {
-            return {
-                "label": label,
-                "description": description,
-                "picked": picked
-            };
-        };
-        return new Promise((resolve) => {
-            vscode.window.showQuickPick([
-                createItem("8.0", "Business Central 2021 release wave 2", true),
-                createItem("7.0", "Business Central 2021 release wave 1"),
-                createItem("6.0", "Business Central 2020 release wave 2"),
-                createItem("5.0", "Business Central 2020 release wave 1"),
-                createItem("4.0", "Business Central 2019 release wave 2"),
-                createItem("3.0", "Business Central Spring '19 Release"),
-                createItem("2.0", "Business Central Fall '18 Release"),
-                createItem("1.0", "Business Central Spring '18 Release")
-            ], options).then(target => {
-                resolve([dir, target.label]);
-            });
-        });
+        /*
+        
+        chooseTargetPlatform(dir) {
+                const options = ({
+                    canPickMany: false,
+                    ignoreFocusOut: true,
+                    placeHolder: resources_1.default.targetPlatformPlaceholder
+                });
+                const createItem = (label, description, picked = false) => {
+                    return {
+                        "label": label,
+                        "description": description,
+                        "picked": picked
+                    };
+                };
+                return new Promise((resolve) => {
+                    vscode.window.showQuickPick([
+                        createItem("8.0", "Business Central 2021 release wave 2", true),
+                        createItem("7.0", "Business Central 2021 release wave 1"),
+                        createItem("6.0", "Business Central 2020 release wave 2"),
+                        createItem("5.0", "Business Central 2020 release wave 1"),
+                        createItem("4.0", "Business Central 2019 release wave 2"),
+                        createItem("3.0", "Business Central Spring '19 Release"),
+                        createItem("2.0", "Business Central Fall '18 Release"),
+                        createItem("1.0", "Business Central Spring '18 Release")
+                    ], options).then(target => {
+                        resolve([dir, target.label]);
+                    });
+                });
+            }
+        
+                let cops: Map<Cops, string> = new Map();
+                cops.set(Cops.AA0206, 'AA0206 - The value assigned to a variable must be used, otherwise the variable is not necessary.')
+                cops.set(Cops.AA0137, 'AA0137 - Do not declare variables that are unused.')
+                cops.set(Cops.AA0008, 'AA0008 - Function calls should have parenthesis even if they do not have any parameters.')
+        
+                let whichCop: string | undefined = await window.showQuickPick(Array.from(cops.values()), { placeHolder: 'Which warning do you want to fix?' })
+                let impl: IInit;
+                switch (whichCop) {
+                    case cops.get(Cops.AA0206):
+                        impl = new CommandFixAssignedButUnusedVariableAA0206()
+                        break;
+                    case cops.get(Cops.AA0137):
+                        impl = new CommandFixUnusedVariablesAA0137()
+                        break;
+                    case cops.get(Cops.AA0008):
+                        impl = new CommandFixMissingParenthesesAA0008();
+                        break;
+                    default:
+                        return;
+                }
+        
+                impl.resolve();
+        */
     }
 
-        let cops: Map<Cops, string> = new Map();
-        cops.set(Cops.AA0206, 'AA0206 - The value assigned to a variable must be used, otherwise the variable is not necessary.')
-        cops.set(Cops.AA0137, 'AA0137 - Do not declare variables that are unused.')
-        cops.set(Cops.AA0008, 'AA0008 - Function calls should have parenthesis even if they do not have any parameters.')
+    private listFilesInDirectory(directoryPath: string, FilesToCopyMap: Map<string, string>) {
+        const files = fs.readdirSync(directoryPath);
 
-        let whichCop: string | undefined = await window.showQuickPick(Array.from(cops.values()), { placeHolder: 'Which warning do you want to fix?' })
-        let impl: IInit;
-        switch (whichCop) {
-            case cops.get(Cops.AA0206):
-                impl = new CommandFixAssignedButUnusedVariableAA0206()
-                break;
-            case cops.get(Cops.AA0137):
-                impl = new CommandFixUnusedVariablesAA0137()
-                break;
-            case cops.get(Cops.AA0008):
-                impl = new CommandFixMissingParenthesesAA0008();
-                break;
-            default:
-                return;
+        var path = require('path');
+
+        for (const file of files) {
+            const fromPath = path.join(directoryPath, file);
+            const stat = fs.statSync(fromPath);
+            if (!stat.isDirectory()) {
+                OwnConsole.ownConsole.appendLine("- " + fromPath);
+                FilesToCopyMap.set("dest "+fromPath,fromPath);
+            } else {
+                if (file != '.noob') {
+                FilesToCopyMap.set("dest " + fromPath, fromPath);
+                OwnConsole.ownConsole.appendLine("- "+ fromPath + " (dir)");
+                this.listFilesInDirectory(fromPath, FilesToCopyMap);
+                }
+            }
+            OwnConsole.ownConsole.show();
         }
-
-        impl.resolve();
-*/
+ 
     }
 }
